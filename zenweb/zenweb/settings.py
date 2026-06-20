ff64fb4c3cd268ce7477d860beff6a1a65eb4dc9
rkenbperez <juicyken068@gmail.com>
2026-06-20 14:03:58 +0800

    added mcstatus api

--- zenweb/zenweb/settings.py
+++ zenweb/zenweb/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'zen',
+    
 ]
 
 MIDDLEWARE = [
@@ -62,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # 'zen.context_processors.server_status',
             ],
         },
     },
